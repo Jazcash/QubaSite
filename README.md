68ede80e373f81e243c211bfe329ecb667e20b4b
Jazcash <jaspercashmore@gmail.com>
2016-04-04 16:37:03 +0100

    Initial files

---
+++ README.md
@@ -0,0 +1,8 @@
+# Quba Site
+
+## Installation
+
+`npm install -g bower gulp`
+`npm install`
+`bower install`
+`gulp`
---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp'),
+	nodemon = require('gulp-nodemon'),
+	plumber = require('gulp-plumber'),
+	livereload = require('gulp-livereload'),
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function () {
+	gulp.src('./public/css/*.scss')
+		.pipe(plumber())
+		.pipe(sass())
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('./public/css'))
+		.pipe(livereload());
+});
+
+gulp.task('watch', function() {
+	gulp.watch('./public/css/*.scss', ['sass']);
+});
+
+gulp.task('develop', function () {
+	livereload.listen();
+	nodemon({
+		script: 'app.js',
+		ext: 'js coffee handlebars',
+		stdout: false
+	}).on('readable', function () {
+		this.stdout.on('data', function (chunk) {
+			if(/^Express server listening on port/.test(chunk)){
+				livereload.changed(__dirname);
+			}
+		});
+		this.stdout.pipe(process.stdout);
+		this.stderr.pipe(process.stderr);
+	});
+});
+
+gulp.task('default', [
+	'sass',
+	'develop',
+	'watch'
+]);
