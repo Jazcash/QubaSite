8d0003122435e832ae981688845359d0fcf55ddc
Jazcash <jaspercashmore@gmail.com>
2016-04-06 13:29:48 +0100

    Parallax more or less working

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
+/*
+	DEXTERITY FRAMES - START
+*/
 $(document).ready(function(){
-	// Dexterity frames
 	for (var i=1; i<72; i++){
 		$("#frames").append("<li class='frame' style='display: none; background-image: url(\"/img/frames/"+i+".jpg\");''/>")
 	}
@@ -8,15 +10,19 @@ $(document).ready(function(){
 });
 
 $(window).load(function(){
-	// Dexterity frames
+	var dexterityHeight = $(".block--hp-dexterity").outerHeight();
+	var dexTop = $(".block--hp-dexterity").offset().top;
+	var dexBottom = dexTop + dexterityHeight;
+	var increment = ($(".block--hp-dexterity").outerHeight() * 2) / 72;
+
 	function switchFrame(frame) {
-	    $('.frame').hide();
-	    $('.frame').eq(frame).show();
+		$('.frame').hide();
+		$('.frame').eq(frame).show();
 	}
 
 	function updateFrame(){
-		var scrollPosTop = $(this).scrollTop();
-		var scrollPosBottom = $(this).scrollTop() + $(window).outerHeight();
+		var scrollPosTop = $(window).scrollTop();
+		var scrollPosBottom = $(window).scrollTop() + dexterityHeight;
 
 		if (scrollPosBottom > dexTop && scrollPosTop < dexBottom){
 			var relativePos = Math.floor(scrollPosBottom - dexTop);
@@ -25,14 +31,48 @@ $(window).load(function(){
 		}
 	}
 
-	var dexterityHeight = $(".block--hp-dexterity").outerHeight();
-    var dexTop = $(".block--hp-dexterity").offset().top;
-    var dexBottom = dexTop + dexterityHeight;
-    var increment = ($(".block--hp-dexterity").outerHeight() * 2) / 72;
-
-    $(window).on('scroll touchmove',function  (event) {
+    $(window).on('scroll touchmove', function(event) {
     	updateFrame();
     });
 
     updateFrame();
 });
+/*
+	DEXTERITY FRAMES - END
+*/
+
+/*
+	OUR WORK OVERLAY/UNDERLAY - START
+*/
+$(function(){
+	$('.hp-our-work').height($('.overlay').outerHeight() + $('.underlay').height(), 500);
+});
+
+$(window).load(function(){
+	$(window).on('scroll touchmove', function(event){
+		var currentScroll = $(this).scrollTop();
+
+		if ($('.hp-our-work').length) {
+		    $('.hp-our-work').height($('.overlay').outerHeight() + $('.underlay').height(), 500);
+
+		    if (($('.overlay').offset().top <= (currentScroll + $(".block--hp-dexterity").outerHeight())) &&
+		        !($('.block--hp-our-partners').offset().top <= (currentScroll))) {
+		        $('.underlay').addClass('show');
+		    } else {
+		        $('.underlay').removeClass('show');
+		    };
+
+		    var _overlay = $('.overlay');
+
+		    if (!(($('.overlay').offset().top + _overlay.find('img').outerHeight()) >= (currentScroll))) {
+		        $('.underlay').addClass('return-scroll');
+		        $('.underlay').addClass('show');
+		    } else {
+		        $('.underlay').removeClass('return-scroll');
+		    };
+		}
+	});
+});
+/*
+	OUR WORK OVERLAY/UNDERLAY - END
+*/
